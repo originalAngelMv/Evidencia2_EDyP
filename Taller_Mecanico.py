56251fe1834f96c445999a2c483734d1d945db4b
Alumno-2026415 <143697133+Alumno-2026415@users.noreply.github.com>
2023-09-21 17:56:04 -0600

    Update Taller_Mecanico.py
    Agregué nuevas opciones

--- Taller_Mecanico.py
+++ Taller_Mecanico.py
@@ -168,9 +168,143 @@ while True:
     elif opcion=="2":#MONTERO CASTILLO DAVID EDUARDO
         pass
     elif opcion=="3":#GONZALEZ INFANTE ALAN JAIR
-        pass
+        while True:
+            folio_cancelar = input("\nIngrese el folio de la nota a cancelar/ 0 para ingresar al menú principal: ").strip()
+            
+            if folio_cancelar=="":
+                print("EL DATO NO PUEDE OMITIRSE. INTENTE DENUEVO.")
+                continue
+
+            if folio_cancelar=="0":
+                break
+
+            try:
+                folio_cancelar=int(folio_cancelar)
+                
+                if not folio_cancelar in notas:
+                    print("\nNOTA NO ENCONTRADA EN EL SISTEMA.")
+                    continue
+            except Exception:
+                print("CARÁCTER NO VÁLIDO. SOLO DÍGITOS NUMÉRICOS")
+                continue
+
+            if notas [folio_cancelar][6]==True:
+                print("\n**************************")
+                print("         NOTA")
+                print("**************************")
+                print(f"Folio: {folio_cancelar:04}")
+                print(f"Fecha de la nota: {notas[folio_cancelar][0].strftime('%d-%m-%Y')}")
+                print(f"Cliente: {notas[folio_cancelar][1]}")
+                print(f"RFC: {notas[folio_cancelar][2]}")
+                print(f"Correo electrónico: {notas[folio_cancelar][3]}")
+                print("\nDetalles de los servicios:")
+                for servicio, costo in notas[folio_cancelar][4]:
+                    print(f"  - {servicio:<20}: ${costo:.2f}")
+                print("-------------------------------")
+                print(f"Monto total:      ${notas[folio_cancelar][5]:.2f}")
+                print("**********************")
+            else:
+                print("NOTA NO ENCONTRADA EN EL SISTEMA.")
+                continue
+            while True:
+
+                confirmacion = input("¿Confirmar la cancelación de esta nota? (s/n): ").lower().strip()
+                
+                if confirmacion=="":
+                    
+                    print("EL DATO NO PUEDE OMITIRSE. INTENTE NUEVAMENTE.")
+                elif confirmacion== "s":
+
+                    notas[folio_cancelar][6] = False
+                    print(f"\nNota con folio {folio_cancelar} ha sido cancelada.\n")
+                    break
+                elif confirmacion=="n":
+                    
+                    print("\nNOTA NO CANCELADA\n")
+                    break
+                else:
+                    
+                    print("OPCIÓN NO VÁLIDA. INTENTE NUEVAMENTE.")
+            break
+        
     elif opcion=="4":#GONZALEZ INFANTE ALAN JAIR
-        pass
+        encontradas_canceladas = False
+
+        for folio, nota in notas.items():
+            if nota[6] == False:
+                encontradas_canceladas = True
+
+        if not encontradas_canceladas:
+            print("\nNO SE ENCONTRARON NOTAS EN EL SISTEMA.\n")
+            continue
+        print("Folio   |   Fecha        |   Cliente              |   RFC          |   Correo                        |   Monto")
+        print("-" * 100)
+
+        for folio, nota in notas.items()
+            if nota[6]==False:
+                fecha = nota[0].strftime('%d-%m-%Y')
+                cliente = nota[1]
+                rfc = nota[2]
+                correo = nota[5]
+                monto = nota[5]
+                
+                cliente = clienteljust(23)
+                rfc = rfc.ljust(15)
+                correo = correo.ljust(30)
+                
+                print(f"{folio:<7} |   {fecha:<12} |   {cliente} |   {rfc} |   {correo} |   ${monto:.2f}")
+
+        while True:
+
+            folio_rescate = input("\nNota a recuperar(Indique número de folio)/0 para volver al menú principal:\n ").strip()
+            
+            if folio_rescate=="":
+                print("EL DATO NO PUEDE OMITIRSE. INTENTE DENUEVO.")
+                continue
+                
+            elif folio_rescate=="0":
+                break
+            else:
+                try:
+                    folio_rescate=int(folio_rescate)
+                    if not folio_rescate in notas:
+                        print("NOTA NO ENCONTRADA. INTENTE NUEVAMENTE")
+                        continue
+                except ValueError:
+                    print("Ingrese un número de folio válido.\n")
+                    continue
+            if folio_rescate in notas and notas[folio_rescate][6]==False:
+                print("\n**************************")
+                print("         NOTA")
+                print("**************************")
+                print(f"Folio: {folio_rescate:04}")
+                print(f"Fecha de la nota: {notas[folio_rescate][0].strftime('%d-%m-%Y')}")
+                print(f"Cliente: {notas[folio_rescate][1]}")
+                print(f"RFC: {notas[folio_rescate][2]}")
+                print(f"Correo electrónico: {notas[folio_rescate][3]}")
+                print("\nDetalles de los servicios.:")
+                for servicio, costo in notas[folio_rescate][4]:
+                    print(f"  - {servicio:<20}: ${costo:.2f}")
+                print("**************************")
+                while True:
+                    confirmacion = input("¿Confirmar la recuperación de esta nota? (s/n): ").lower().strip()
+                    
+                    if confirmacion=="":
+                     print("NO SE PUEDE OMITIR EL DATO")
+                     continue
+                    elif confirmacion=="n":
+                        print("\nNOTA NO FUE RECUPERADA\n")
+                        break
+                    elif confirmacion=="s":
+                        notas[folio_rescate][6] = True
+                        print(f"\nNota con folio {folio_rescate:04} ha sido recuperada.\n")
+                        break
+                    else:
+                        print("OPCIÓN NO VÁLIDA")
+                        continue
+               break
+    else:
+        print("FOLIO NO ENCONTRADO EN EL SISTEMA")
     elif opcion=="5":#ANGEL MORALES VENTURA
         if input("SEGURO DESEA SALIR DEL PROGRAMA? (S/N o Enter para volver a menu principal):\n ").upper()=="S":
             break
